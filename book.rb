be79f0da182207388c34cf099bfde224dfa915b5
Bludidi <bludidi5@gmail.com>
2023-02-01 18:39:43 +0200

    Create book class

--- book.rb
+++ book.rb
@@ -1,4 +1,17 @@
-class book
-  
+require './item'
 
+class Book < Item
+  attr_accessor :title, :publish_date, :cover_state, :publisher
+
+  def initialize(publisher:, cover_state:, publish_date:, title: 'unknown', archived: false)
+    super(publish_date: publish_date, archived: archived, title: title)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+
+  private :can_be_archived?
 end
