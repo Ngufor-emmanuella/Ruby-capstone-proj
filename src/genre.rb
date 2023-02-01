96b08d2682341ae6450e620bb623cf5220e8bde0
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-01 13:57:25 +0100

    Add semi colon to author file

---
+++ src/genre.rb
@@ -0,0 +1,22 @@
+require_relative './items'
+
+class Genre
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name, id: nil)
+    @id = id.nil? generate_id : id
+    @name = name.to_s
+    @items = []
+  end
+
+  def add_items(item)
+    @items << item
+    item.genre = self
+  end
+
+  def generate_id
+    rand(i..1000)
+  end
+end
+
