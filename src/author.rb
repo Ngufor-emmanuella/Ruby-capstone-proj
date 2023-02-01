9e54cd8c25ef12e320f4dbc03f23e8414fcb78d6
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-01 13:21:12 +0100

    Add music class

--- src/author.rb
+++ src/author.rb
@@ -1,15 +1,151 @@
 class Author
-  attr_accessor :first_name, :last_name, :items
+  attr_accessor :first_name, :last_name, :items_array
 
   def initialize(first_name, last_name)
     @id = Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
-    @items = []
-  end
+    @items_array = []
 
-  def add_item(item)
-    @items << item
-    item.author = self
-  end
-end
+    def add_item(item)
+      @items_array << item
+      item.author = self
+    end
+  end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
