5e8ae46b529e61cf6c1220d4c94357ad376842ce
Bludidi <bludidi5@gmail.com>
2023-02-01 12:37:33 +0200

    Create app entry point

---
+++ app.rb
@@ -0,0 +1,19 @@
+class App
+  def show_menu
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts
+    puts '1) - List all books'
+    puts '2) - List all music albums'
+    puts '3) - List of games'
+    puts "4) - List all labels (e.g 'Gift', 'New')"
+    puts "5) - List all authors ( e.g 'Stephen King')"
+    puts "6) - List all sources ( e.g 'from a friend', 'online shop')"
+    puts '7) - Add a book'
+    puts '8) - Add a music album'
+    puts '9) - Add a game'
+    puts '0) - Exit'
+    puts '============================================================'
+    puts
+  end
+end
