f9644da5237681d807ab8f0482dbcb3d06b31905
Bludidi <bludidi5@gmail.com>
2023-02-02 13:46:22 +0200

    Create label spec file

---
+++ label_spec.rb
@@ -0,0 +1,23 @@
+require_relative 'spec_helper'
+
+describe 'Label' do
+  before :each do
+    @label = Label.new(title: 'Romance', color: 'Red')
+    @item = Item.new(name: 'Serendipity', publish_date: '2011-12-2', archived: false)
+  end
+
+  describe 'This is instantiated in before(:each)' do
+    it 'is an instance of Label' do
+      expect(@label).to be_an_instance_of(Label)
+    end
+
+    it 'has items as an empty array' do
+      expect(@label.items).to be_empty
+    end
+
+    it 'adds an item to items' do
+      @label.add_item(@item)
+      expect(@label.items[0]).to be(@item)
+    end
+  end
+end
