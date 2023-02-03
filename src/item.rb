fc5ab0fc70d125ede9587f2acbde652a2d92ee5c
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-04 00:55:47 +0100

    Fix linters in units test

--- src/item.rb
+++ src/item.rb
@@ -3,6 +3,7 @@ require 'date'
 class Item
   attr_accessor :publish_date, :archived, :label
   attr_reader :id
+
   def initialize(publish_date, archived: false)
     @id = rand(100...1000)
     @publish_date = publish_date
@@ -36,4 +37,3 @@ class Item
     @archived = true if can_be_archived?
   end
 end
-
