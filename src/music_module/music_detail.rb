23ad0ffb4369bd2f3cd0d71cab37d264ca80d59b
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-03 19:56:31 +0100

    Additional Requirements

---
+++ src/music_module/music_detail.rb
@@ -0,0 +1,23 @@
+require_relative './music_album'
+
+class MusicDetail < MusicAlbum
+  @@musicAlbums = []
+  attr_reader :title, :artist
+
+  def initialize(title, artist, genre, on_spotify, publish_date)
+    super(publish_date, on_spotify)
+    @title = title
+    @artist = artist
+    @genre = genre
+
+    @@musicAlbums << self
+  end
+
+  def self.list_music_albums
+    @@musicAlbums
+  end
+end
+
+
+
+
