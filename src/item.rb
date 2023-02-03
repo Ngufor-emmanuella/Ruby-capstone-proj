1265d46b8a655119ef22b569a2ec393a050fb110
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-03 20:28:02 +0100

    Add endline

--- src/item.rb
+++ src/item.rb
@@ -3,6 +3,7 @@ require 'date'
 class Item
   attr_accessor :publish_date, :archived, :label
   attr_reader :id
+
   def initialize(publish_date, archived: false)
     @id = rand(100...1000)
     @publish_date = publish_date
