228bd5dd08861f1b694b5fe85dfa10c9627aafd9
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-04 10:36:51 +0100

    Add solutions

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,31 +1,12 @@
 require_relative '../src/genre'
 require_relative '../src/item'
-require 'rspec'
-
 describe Genre do
-  describe 'class' do
-    it 'creates a new object of Genre name' do
-      genre = Genre.new('emma')
-      expect(genre.name).to eq('emma')
-    end
+  context 'when testing genre class mthods' do
+    name = 'rock'
+    genre = Genre.new(name)
 
-    it 'crreates a new object of Genre name' do
-      genre = Genre.new('emma')
-      expect(genre.items).to eq([])
-    end
-
-    it 'generates random id' do
-      genre = Genre.new('emma')
-      allow(genre).to recieve(:rand).and_return(5)
-      result = genre.send(:generate_id)
-      expect(result).to eq(5)
-    end
-
-    it 'adds random items' do
-      item = Item.new('archived', DateTime.now.to_s)
-      genre = Genre.new('cesar', id: 10)
-      genre.add_item(item)
-      expect(genre.items).to include(item)
+    it 'returns name instance of the genre class ' do
+      expect(genre.name).to equal(name)
     end
   end
 end
