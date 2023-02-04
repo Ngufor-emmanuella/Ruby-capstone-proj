961e23dea6f85116af4a19fbe1aeed570303fec7
Bludidi <bludidi5@gmail.com>
2023-02-04 13:41:14 +0200

    Fix rubocop errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,14 +3,14 @@ require_relative '../label'
 require_relative '../src/item'
 require_relative 'spec_helper'
 
-describe 'Test the book class' do 
+describe 'Test the book class' do
   book = Book.new('Book1', 'Penguin', 'Good', '2020-05-23')
 
   it 'returns the right book publisher' do
     expect(book.publisher).to eql 'Penguin'
   end
 
-  it 'returns the correct publish date' do 
+  it 'returns the correct publish date' do
     expect(book.publish_date).to eql '2020-05-23'
   end
 
