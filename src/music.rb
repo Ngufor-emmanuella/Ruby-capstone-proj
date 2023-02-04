8356e898c89de1161b045c2003a9599a374cbaa0
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-02-04 11:09:52 +0100

    Fix testing errors

--- src/music.rb
+++ src/music.rb
@@ -4,8 +4,8 @@ class Music < Item
   attr_reader :id
   attr_accessor :on_spotify
 
-  def initialize(on_spotify, published_date, archived: false, id: nil)
-    super(id, published_date, archived)
+  def initialize(on_spotify, publish_date, archived: false, id: nil)
+    super(id, publish_date, archived)
     @on_spotify = on_spotify
   end
 
